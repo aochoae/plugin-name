ca8690c96b968d29fe74dc96d1018dff2916acd6
Luis Alberto Ochoa Esparza <alberto.0x7c3@gmail.com>
2019-02-16 00:57:12 -0600

    Remove PluginName directory

--- plugin-name/autoloader.php
+++ plugin-name/autoloader.php
@@ -19,7 +19,7 @@ spl_autoload_register(function ($class) {
     $prefix = 'PluginName\\';
 
     // base directory for the namespace prefix
-    $base_dir = __DIR__ . '/src/PluginName/';
+    $base_dir = __DIR__ . '/src/';
 
     // does the class use the namespace prefix?
     $len = strlen($prefix);
